fa3e8922482a49de9096bae5ca8e8b0a27ba382e
mittalabhyu <mittalabhyudaya@gmail.com>
2025-01-28 22:15:45 +0530

    Home page for expense tracker

---
+++ expense_tracker/src/components/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import img1 from '../asset/img/dashboard.png'
+function Homepage() {
+    return (
+        <div className='container'>
+            <div className='leftcontainer'>
+          
+                <img src={img1} />
+
+            </div>
+            <div className='rightcontainer'>
+               
+            </div>
+
+        </div>
+    )
+}
+
+export default Homepage
