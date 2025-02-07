ed1a0dfa7e2186d203ecce2f111e23c650a356bf
mittalabhyu <mittalabhyudaya@gmail.com>
2025-02-07 22:45:16 +0530

    Dashboard UI changes

--- expense_tracker/src/components/Dashboard.js
+++ expense_tracker/src/components/Dashboard.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import img1 from '../asset/img/cart.jpg'
 import { Link, useNavigate } from 'react-router-dom'
 function Dashboard() {
+    const myExpenses = [100,200,300,600,700,800];
 
     const navigate = useNavigate();
     const [inputs, setInputs] = useState({})
-    const [showerror, setShowError] = useState(false)
+    const [showdata, setShowData] = useState(false)
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
@@ -13,71 +14,101 @@ function Dashboard() {
     }
 
 
-    const doLogin = (event) => {
+    const addExpense = (event) => {
         event.preventDefault();
         console.log("Clicked", inputs);
-
-        if (inputs.pass.length >= 8) {
-            navigate("/dashboard");
-        }
-        else {
-            setShowError(true);
-        }
+        setShowData(true)
     }
     return (
         <div className='container'>
-            <div className='leftcontainer'>
-                <p>Please add your expenses</p>
-                <img src={img1} />
+            {showdata ?
+                <div className='leftcontainer'>
+                    <p>Total Expenses : 1000</p>
+                    {myExpenses.map((val) =>
+                        <div className='expenseCard'>
+                            <p>Title</p>
+                            <p>Amount {val}</p>
+                            <p>Date</p>
+                            <p>Type</p>
+                        </div>
+                    )}
+                </div>
+                :
+                <div className='leftcontainer'>
+                    <p>Please add your expenses</p>
+                    <img src={img1} />
+                </div>
+            }
 
-            </div>
             <div className='rightcontainer'>
                 <div className='formcard'>
                     <div>
                         <h2 style={{ alignSelf: 'center' }}>Add Expense</h2>
                     </div>
-                    <form onSubmit={doLogin}>
+                    <form onSubmit={addExpense}>
                         <div>
                             <label>
-                                Email
+                                Title
                             </label>
                             <input
                                 required
-                                type="email"
-                                placeholder='Enter your email'
-                                value={inputs.email || ''}
+                                type="text"
+                                placeholder='Enter your expense title'
+                                value={inputs.title || ''}
                                 onChange={handleChange}
-                                name='email'
+                                name='title'
 
                             />
                         </div>
                         <div>
                             <label>
-                                Password
+                                Amount
                             </label>
                             <input
                                 required
-                                type="password"
-                                placeholder='Enter your Password'
-                                value={inputs.pass || ''}
+                                type="number"
+                                placeholder='Enter your expense amount'
+                                value={inputs.expense || ''}
                                 onChange={handleChange}
-                                name='pass'
+                                name='expense'
 
                             />
                         </div>
-                        {
-                            showerror ?
-                                <div>
-                                    <span style={{ color: 'red', alignSelf: 'center' }}>Password length must be greater then 8</span>
-                                </div> : null
-                        }
+                        <div>
+                            <label>
+                                Type
+                            </label>
+                            <select
+                                value={inputs.type || ''}
+                                onChange={handleChange}
+                                name='type'
+                            >
+                                <option value="cash">Cash</option>
+                                <option value="upi">UPI</option>
+                                <option value="card">Card</option>
+                            </select>
+
+                        </div>
+                        <div>
+                            <label>
+                                Date
+                            </label>
+                            <input
+                                required
+                                type="date"
+                                placeholder='Enter Date'
+                                value={inputs.date || ''}
+                                onChange={handleChange}
+                                name='date'
+
+                            />
+                        </div>
+
 
                         <div>
                             <button>Add</button>
                         </div>
-                        <div>
-                            <span style={{ alignSelf: 'center' }}>Not a user ?<Link to="/register">Register</Link> </span>
-                        </div>
+
                     </form>
                 </div>
 
