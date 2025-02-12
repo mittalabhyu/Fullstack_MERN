2ff557d38d67412672b12083bcdc3f6ae2c9d18a
mittalabhyu <mittalabhyudaya@gmail.com>
2025-02-12 11:21:33 +0530

    new modals added and ui enhancements

--- expense_tracker/src/components/Dashboard.js
+++ expense_tracker/src/components/Dashboard.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import img1 from '../asset/img/cart.jpg'
 import del from '../asset/img/delete.svg'
 import edit from '../asset/img/edit.png'
 import { Link, useNavigate } from 'react-router-dom'
 function Dashboard() {
-    const myExpenses = [100,200,300,600,700,800];
-
     const navigate = useNavigate();
     const [inputs, setInputs] = useState({})
     const [showdata, setShowData] = useState(false)
@@ -15,7 +13,44 @@ function Dashboard() {
         setInputs(values => ({ ...values, [name]: value }))
     }
 
+    const myExpenses = [100, 200, 300, 600, 700, 800];
+    useEffect(() => {
 
+    },[showdata])
+   
+  
+
+    const deleteExpense = () => {
+        closeModal1()
+    }
+    const openModal1 = () => {
+      
+        var modal1 = document.getElementById("myModal");
+        console.log("AA")
+        modal1.style.display = "block";
+    }
+    const closeModal1 = () => {
+    
+        var modal1 = document.getElementById("myModal");
+        console.log("BB")
+        modal1.style.display = "none";
+    }
+    const editExpense = () => {
+        closeModal2()
+    }
+    const openModal2 = () => {
+     
+        var modal2 = document.getElementById("myModal2");
+        console.log("CC")
+        modal2.style.display = "block";
+    }
+    const closeModal2 = (event) => {
+ 
+   
+        var modal2 = document.getElementById("myModal2");
+        console.log("DD")
+        modal2.style.display = "none";
+    }
     const addExpense = (event) => {
         event.preventDefault();
         console.log("Clicked", inputs);
@@ -26,19 +61,107 @@ function Dashboard() {
             {showdata ?
                 <div className='leftcontainer'>
                     <p>Total Expenses : 1000</p>
-                    <div style={{backgroundColor:'lightgray',width:'100%',overflowY:'scroll',marginBottom:'50px',justifyContent:'center',justifyItems:'center',padding:'20px'}}>
-                    {myExpenses.map((val) =>
-                        <div className='expenseCard'>
-                            <div style={{width:'100%',display:'flex',flexDirection:'row',justifyContent:'flex-end'}}>
-                            <img style={{width:'20px',height:'20px'}} src={edit} />
-                            <img style={{width:'20px',height:'20px',marginLeft:'4px'}} src={del} />
-                            </div>
-                            <p>Title</p>
-                            <p>Amount {val}</p>
-                            <p>Date</p>
-                            <p>Type</p>
+
+                    <div id="myModal" className="modal">
+                        <div className="modal-content">
+                            <p>Do you want to delete this expense?</p>
+                            <button onClick={deleteExpense}>Yes</button>
+                            <button onClick={closeModal1}>No</button>
                         </div>
-                    )}
+                    </div>
+                    <div id="myModal2" className="modal2">
+                        
+                            <div className='formcard'>
+                                <div>
+                                    <h2 style={{ alignSelf: 'center' }}>Edit Expense</h2>
+                                </div>
+                                <form onSubmit={editExpense}>
+                                    <div>
+                                        <label>
+                                            Title
+                                        </label>
+                                        <input
+                                            required
+                                            type="text"
+                                            placeholder='Enter your expense title'
+                                            value={inputs.title || ''}
+                                            onChange={handleChange}
+                                            name='title'
+
+                                        />
+                                    </div>
+                                    <div>
+                                        <label>
+                                            Amount
+                                        </label>
+                                        <input
+                                            required
+                                            type="number"
+                                            placeholder='Enter your expense amount'
+                                            value={inputs.expense || ''}
+                                            onChange={handleChange}
+                                            name='expense'
+
+                                        />
+                                    </div>
+                                    <div>
+                                        <label>
+                                            Type
+                                        </label>
+                                        <select
+                                            value={inputs.type || ''}
+                                            onChange={handleChange}
+                                            name='type'
+                                            required
+
+                                        >
+                                            <option disabled value=''>Please Select Payment Type</option>
+                                            <option value="cash">Cash</option>
+                                            <option value="upi">UPI</option>
+                                            <option value="card">Card</option>
+                                        </select>
+
+                                    </div>
+                                    <div>
+                                        <label>
+                                            Date
+                                        </label>
+                                        <input
+                                            required
+                                            type="date"
+                                            placeholder='Enter Date'
+                                            value={inputs.date || ''}
+                                            onChange={handleChange}
+                                            name='date'
+
+                                        />
+                                    </div>
+
+
+                                    <div>
+                                        <button>Edit Expense</button>
+                                    </div>
+                                    <div>
+                                        <button onClick={closeModal2}>Cancel</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                    
+                    </div>
+                    <div style={{ backgroundColor: 'lightgray', width: '100%', overflowY: 'scroll', marginBottom: '50px', justifyContent: 'center', justifyItems: 'center', padding: '20px' }}>
+                        {myExpenses.map((key,val) =>
+                            <div className='expenseCard' key={key}>
+                                <div style={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', justifyItems: 'center', alignContent: 'center', alignItems: 'center' }}>
+                                    <img onClick={openModal2} style={{ width: '30px', height: '30px' }} src={edit} />
+                                    <img onClick={openModal1} style={{ width: '20px', height: '20px', marginLeft: '4px' }} src={del} />
+                                </div>
+                                <p>Title</p>
+                                <p>Amount {val}</p>
+                                <p>Date</p>
+                                <p>Type</p>
+                            </div>
+                        )}
                     </div>
                 </div>
                 :
@@ -90,7 +213,10 @@ function Dashboard() {
                                 value={inputs.type || ''}
                                 onChange={handleChange}
                                 name='type'
+                                required
+
                             >
+                                <option disabled value=''>Please Select Payment Type</option>
                                 <option value="cash">Cash</option>
                                 <option value="upi">UPI</option>
                                 <option value="card">Card</option>
--- expense_tracker/src/components/Header.js
+++ expense_tracker/src/components/Header.js
@@ -1,14 +1,92 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Logo from '../asset/img/logo.png'
 import arrow from '../asset/img/downarrow.png'
 import { useNavigate, useLocation } from 'react-router-dom'
 
 function Header() {
-  const navigate = useNavigate();
   const location = useLocation();
+   const [inputs, setInputs] = useState({})
+       const navigate = useNavigate();
+          const handleChange = (event) => {
+              const name = event.target.name;
+              const value = event.target.value;
+              setInputs(values => ({ ...values, [name]: value }))
+          }
+  const doLogout =()=>{
+    navigate('/');
+  }
+  const openModal = () => {
+      
+    var modal = document.getElementById("myModal3");
+    console.log("AA")
+    modal.style.display = "block";
+}
+const closeModal = () => {
+
+    var modal = document.getElementById("myModal3");
+    console.log("BB")
+    modal.style.display = "none";
+}
   console.log("MSG ", location.pathname);
   return (
     <div className='header'>
+       <div id="myModal3" className="modal1">
+       <div className='formcard'>
+                <div>
+                    <h2 style={{ alignSelf: 'center' }}>Change Password</h2>
+                </div>
+                <form onSubmit={closeModal}>
+                    <div>
+                        <label>
+                            Old Password
+                        </label>
+                        <input
+                            required
+                            type="password"
+                            placeholder='Enter your old password'
+                            value={inputs.psw || ''}
+                            onChange={handleChange}
+                            name='psw'
+
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            New Password
+                        </label>
+                        <input
+                            required
+                            type="password"
+                            placeholder='Enter your new password'
+                            value={inputs.npsw || ''}
+                            onChange={handleChange}
+                            name='npsw'
+
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            Confirm New Password
+                        </label>
+                        <input
+                            required
+                            type="password"
+                            placeholder='Confirm your new password'
+                            value={inputs.cnfpsw || ''}
+                            onChange={handleChange}
+                            name='cnfpsw'
+
+                        />
+                    </div>
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                    <div>
+                        <button onClick={closeModal}>Cancel</button>
+                    </div>
+                </form>
+            </div>
+                    </div>
       <div style={{ display: 'flex', width: '90%', alignItems: 'center' }}>
         <img src={Logo} />
         <h2>Expense Tracker</h2>
@@ -16,12 +94,12 @@ function Header() {
       {
         location.pathname === '/dashboard' ?
           <div style={{ width: '10%', display: 'flex', alignItems: 'center' }}>
-            <p className="dropbtn">User Name</p>
+            <p>User Name</p>
             <div className="dropdown">
               <img style={{ width: '10px', height: '10px', marginLeft: '10px' }} src={arrow} />
               <div className="dropdown-content">
-                <p>Change Password</p>
-                <p>Logout</p>
+                <p onClick={openModal}>Change Password</p>
+                <p onClick={doLogout}>Logout</p>
               </div>
             </div>
             {/* <p>User Name</p>
