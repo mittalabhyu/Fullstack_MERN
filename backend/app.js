6655af05543af8c53c4509592a50f9cb88e8999e
mittalabhyu <mittalabhyudaya@gmail.com>
2025-02-14 20:11:03 +0530

    backend init

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.post('/login', (req, res) => {
+    res.send('Logged In!')
+  })
+  
+
+
+app.listen(port, () => {
+  console.log(`Server Running on port ${port}`)
+})
+
